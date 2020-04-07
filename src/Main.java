7b227e2ff4e077b87fd805d149a5c81dc1bbab88
Feras Aloudah <fireslay@gmail.com>
2020-04-07 09:53:54 +0300

    Modified home.fxml a little.

--- src/Main.java
+++ src/Main.java
@@ -25,47 +25,5 @@ public class Main extends Application {
 
     public static void main(String[] args) throws InterruptedException {
         launch(args);
-        NoteParser noteParser = new NoteParser();
-        NoteTracker noteTracker = new NoteTracker();
-        String tests[] = new String[] {
-                "adc f",
-                "jungle t 3",
-                "top e r",
-                "Mid f lb",
-                "supp f lb r",
-                "jg tp lb r 10",
-                "qwer",
-                "top",
-                "f lb r"
-        };
-
-        int x = 2;
-
-        if (x == 1) {
-            Server server = new Server(null, null, "test", 8002);
-            System.out.println("Started Server");
-        }
-
-        if (x == 2) {
-            Client client = new Client();
-            client.connect(null, null,"Feras","localhost", 5555);
-            System.out.println("Client 1: Connected");
-        }
-
-        System.out.println("---------------------------");
-
-        if (x == 3) {
-            Client client = new Client();
-            client.connect(null, null,"Badr", "localhost", 8002);
-            System.out.println("Client 2: Connected");
-            Note note;
-            for (String test : tests) {
-                if ((note = noteParser.parseNote(test)) != null) {
-                    noteTracker.addNote(note);
-                    client.sendNote(note);
-                    TimeUnit.SECONDS.sleep(1);
-                }
-            }
-        }
     }
 }
